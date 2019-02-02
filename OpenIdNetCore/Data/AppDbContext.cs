42b4bf300a38feb97f7427bdade98bbb7393e2e7
TheBinaryGuy <heet_mehta@hotmail.com>
2019-02-02 23:07:32 +0530

    Added Npgsql and migrations...

---
+++ OpenIdNetCore/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OpenIdNetCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
--- OpenIdNetCore/Startup.cs
+++ OpenIdNetCore/Startup.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OpenIdNetCore.Data;
 
 namespace OpenIdNetCore
 {
@@ -23,6 +25,12 @@ namespace OpenIdNetCore
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddEntityFrameworkNpgsql().AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration["PostgresConnString"]);
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
