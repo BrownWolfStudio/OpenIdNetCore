bbb072e20d234a281637deda7f9cb0c228333436
TheBinaryGuy <heet_mehta@hotmail.com>
2019-02-02 19:54:52 +0530

    Initialized Project

---
+++ OpenIdNetCore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenIdNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content(nameof(Index));
+        }
+
+        public IActionResult Error()
+        {
+            return Content(nameof(Error));
+        }
+    }
+}
